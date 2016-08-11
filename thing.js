394d2af0effc53014194ae82d93ceceefc84432a
ClarissaAncira <cmancira76@gmail.com>
2016-08-11 15:43:09 -0700

    My First Thing

---
+++ thing.js
@@ -0,0 +1,12 @@
+function changePhoto(){
+    var img = document.getElementById("hi");
+img.src = "PaddleCode.png";
+}
+function change2Photo(){
+    var img = document. getElementById("square");
+img.src = "SquareCode.png";
+}
+function change3Photo(){
+    var img = document. getElementById("scroller");
+img.src = "ScrollerCode.png";
+}
